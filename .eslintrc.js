7f20ee8a712358ecab796cbb73b6dbb95a173499
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2024-03-26 18:32:57 +0000

    ci: add quality-checking CI config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,9 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@flam/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
+	extends: ["@flam/eslint-config/library.js"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: true,
+	},
 };
