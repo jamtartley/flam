2e886f792af35fb4314b08945e5c0dff1ad7691c
Sam Hartley <sam@jamtartley.com>
2024-03-26 17:07:58 +0000

    chore(compiler): handle strings and idents inside templates

--- apps/compiler/src/tokenizer.ts
+++ apps/compiler/src/tokenizer.ts
@@ -1,4 +1,11 @@
-type TokenKind = "RAW" | "LIT_IDENTIFIER" | "TEMPLATE_START" | "TEMPLATE_END" | "EOF";
+type TokenKind =
+	| "RAW"
+	| "LITERAL_IDENTIFIER"
+	| "LITERAL_STRING"
+	| "TEMPLATE_START"
+	| "TEMPLATE_END"
+	| "OP_PIPE"
+	| "EOF";
 
 type Token = {
 	kind: TokenKind;
@@ -11,12 +18,20 @@ export class Tokenizer {
 
 	constructor(private readonly fileContents: string) {}
 
-	#append(kind: TokenKind, value: string): void {
-		this.tokens.push({ kind, value });
+	#current(length: number = 1): string {
+		const start = this.#index;
+
+		return this.fileContents.slice(start, start + length);
 	}
 
-	#peek(length: number): string {
-		return this.fileContents.slice(this.#index, this.#index + length);
+	#next(length: number = 1): string {
+		const start = this.#index + 1;
+
+		return this.fileContents.slice(start, start + length);
+	}
+
+	#append(kind: TokenKind, value: string): void {
+		this.tokens.push({ kind, value });
 	}
 
 	#advance(length: number): void {
@@ -26,24 +41,62 @@ export class Tokenizer {
 	#tokenizeRaw(): void {
 		const startIndex = this.#index;
 
-		while (this.#peek(2) !== "{=" && this.#index < this.fileContents.length) {
+		while (this.#current(2) !== "{=" && this.#index < this.fileContents.length) {
 			this.#advance(1);
 		}
 
 		this.#append("RAW", this.fileContents.slice(startIndex, this.#index));
 	}
 
-	#tokenizeTemplate(): void {
-		this.#append("TEMPLATE_START", "=}");
-		this.#advance(2);
-
+	#tokenizeIdent(): void {
 		const startIndex = this.#index;
+		const endRegex = /[^a-zA-Z0-9_]/;
 
-		while (this.#peek(2) !== "{=" && this.#index < this.fileContents.length) {
+		while (!endRegex.test(this.#current())) {
 			this.#advance(1);
 		}
 
-		this.#append("LIT_IDENTIFIER", this.fileContents.slice(startIndex, this.#index));
+		this.#append("LITERAL_IDENTIFIER", this.fileContents.slice(startIndex, this.#index));
+	}
+
+	#tokenizeString(): void {
+		this.#advance(1);
+
+		const startIndex = this.#index;
+		while (this.#current() !== '"' && this.#index < this.fileContents.length) {
+			this.#advance(1);
+		}
+
+		console.log("HER");
+		this.#advance(1);
+		this.#append("LITERAL_STRING", this.fileContents.slice(startIndex, this.#index));
+	}
+
+	#tokenizeTemplate(): void {
+		this.#append("TEMPLATE_START", "{=");
+		this.#advance(2);
+
+		while (this.#current(2) !== "=}" && this.#index < this.fileContents.length) {
+			switch (this.#current()) {
+				case " ":
+				case "\n":
+				case "\t":
+					this.#advance(1);
+					continue;
+				case "|":
+					if (this.#next() === ">") {
+						this.#append("OP_PIPE", "|>");
+						this.#advance(2);
+						continue;
+					}
+				case '"':
+					this.#tokenizeString();
+					continue;
+				default:
+					this.#tokenizeIdent();
+					continue;
+			}
+		}
 
 		this.#append("TEMPLATE_END", "=}");
 		this.#advance(2);
@@ -51,10 +104,12 @@ export class Tokenizer {
 
 	public tokenize(): Tokenizer {
 		while (this.#index < this.fileContents.length) {
-			switch (this.#peek(2)) {
-				case "{=":
-					this.#tokenizeTemplate();
-					continue;
+			switch (this.#current()) {
+				case "{":
+					if (this.#next() === "=") {
+						this.#tokenizeTemplate();
+						continue;
+					}
 				default:
 					this.#tokenizeRaw();
 					continue;
