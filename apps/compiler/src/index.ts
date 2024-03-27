4a1a2baead0d07f056377740dfde9e3e00a0f76a
Sam Hartley <sam@jamtartley.com>
2024-03-27 17:22:43 +0000

    chore(compiler): add 'flammify' filter

--- apps/compiler/src/index.ts
+++ apps/compiler/src/index.ts
@@ -2,7 +2,7 @@ import { Compiler } from "./compiler";
 import { Parser } from "./parser";
 import { Tokenizer } from "./tokenizer";
 
-const tokenizer = new Tokenizer('{= "Hello" |> uppercase =}').tokenize();
+const tokenizer = new Tokenizer('{= "The quick brown fox jumps over the lazy dog" |> flammify =}').tokenize();
 const parser = new Parser(tokenizer.tokens).parse();
 const compiler = new Compiler(parser.rootNode);
 
