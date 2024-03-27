3876bad7a471ac32767f70bcb2650cb47bb8491b
Sam Hartley <sam@jamtartley.com>
2024-03-27 17:19:01 +0000

    chore(compiler): add filter validation with io-ts

--- apps/compiler/src/index.ts
+++ apps/compiler/src/index.ts
@@ -2,7 +2,7 @@ import { Compiler } from "./compiler";
 import { Parser } from "./parser";
 import { Tokenizer } from "./tokenizer";
 
-const tokenizer = new Tokenizer("{= 30 |> mult =}").tokenize();
+const tokenizer = new Tokenizer('{= "Hello" |> uppercase =}').tokenize();
 const parser = new Parser(tokenizer.tokens).parse();
 const compiler = new Compiler(parser.rootNode);
 
