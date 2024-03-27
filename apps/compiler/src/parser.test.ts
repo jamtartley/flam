3b276d8d7f6c7f8e5940e3836f9a49eb173ce106
Sam Hartley <sam@jamtartley.com>
2024-03-27 10:41:34 +0000

    chore(compiler): throw on unexpected tokens during parse

--- apps/compiler/src/parser.test.ts
+++ apps/compiler/src/parser.test.ts
@@ -27,3 +27,16 @@ test("Parser emits an AstTemplateNode", () => {
 	assert.ok(parser.rootNode.statements[0] !== undefined);
 	assert.ok(parser.rootNode.statements[0].kind === "AstTemplateNode");
 });
+
+test("Parser throws an UnexpectedTokenError if starting with a TEMPLATE_END token", () => {
+	const tokens: Token[] = [
+		{ kind: "TEMPLATE_END", value: "=}", site: { line: 1, col: 1 } },
+		{ kind: "EOF", value: "", site: { line: 1, col: 3 } },
+	];
+	const parser = new Parser(tokens);
+
+	assert.throws(() => parser.parse(), {
+		name: "UnexpectedTokenError",
+		message: "Unexpected token - expected: TEMPLATE_START but got TEMPLATE_END",
+	});
+});
