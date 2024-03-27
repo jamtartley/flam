f3f55834eb7f0d1e022c70d08ee67956eb0d762b
Sam Hartley <sam@jamtartley.com>
2024-03-27 10:25:13 +0000

    chore(compiler): stub out Parser with root node

---
+++ apps/compiler/src/parser.test.ts
@@ -0,0 +1,29 @@
+import assert from "node:assert/strict";
+import { test } from "node:test";
+import { Token } from "./tokenizer";
+import { Parser } from "./parser";
+
+test("Parser emits a single AstRootNode", () => {
+	const tokens: Token[] = [
+		{ kind: "TEMPLATE_START", value: "{=", site: { line: 1, col: 1 } },
+		{ kind: "LITERAL_NUMBER", value: "42", site: { line: 1, col: 4 } },
+		{ kind: "TEMPLATE_END", value: "=}", site: { line: 1, col: 6 } },
+		{ kind: "EOF", value: "", site: { line: 1, col: 8 } },
+	];
+	const parser = new Parser(tokens).parse();
+
+	assert.ok(parser.rootNode !== undefined);
+});
+
+test("Parser emits an AstTemplateNode", () => {
+	const tokens: Token[] = [
+		{ kind: "TEMPLATE_START", value: "{=", site: { line: 1, col: 1 } },
+		{ kind: "LITERAL_NUMBER", value: "42", site: { line: 1, col: 4 } },
+		{ kind: "TEMPLATE_END", value: "=}", site: { line: 1, col: 6 } },
+		{ kind: "EOF", value: "", site: { line: 1, col: 8 } },
+	];
+	const parser = new Parser(tokens).parse();
+
+	assert.ok(parser.rootNode.statements[0] !== undefined);
+	assert.ok(parser.rootNode.statements[0].kind === "AstTemplateNode");
+});
