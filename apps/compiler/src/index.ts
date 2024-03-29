5368193757316580aa7834b7fac7173c4085daa1
Sam Hartley <sam@jamtartley.com>
2024-03-29 14:48:20 +0000

    chore(compiler): convert JS object to Context

--- apps/compiler/src/index.ts
+++ apps/compiler/src/index.ts
@@ -1,19 +1,13 @@
-import { ArrayValue, Compiler, ValueKind } from "./compiler";
+import { Compiler } from "./compiler";
 import { Context } from "./context";
 import { Parser } from "./parser";
 import { Tokenizer } from "./tokenizer";
 
 const tokenizer = new Tokenizer('{= names |> join(", ") =}').tokenize();
 const parser = new Parser(tokenizer.tokens).parse();
-const context = new Context();
-context.add("names", {
-	kind: ValueKind.ARRAY,
-	value: [
-		{ kind: ValueKind.STRING, value: "Cameron" },
-		{ kind: ValueKind.STRING, value: "Donna" },
-		{ kind: ValueKind.STRING, value: "Gordon" },
-	],
-} as ArrayValue);
+const context = Context.fromObj({
+	names: ["Cameron", "Donna", "Gordon"],
+});
 const compiler = new Compiler(parser.rootNode, context);
 
 console.log(compiler.compile());
