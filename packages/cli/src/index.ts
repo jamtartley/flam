66e5cca54d6753647c9a20766906a4aac173e40e
Sam Hartley <11541019+jamtartley@users.noreply.github.com>
2024-03-30 21:28:59 +0000

    refactor: separate CLI from compiler

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,35 @@
+import { readFileSync, writeFileSync } from "node:fs";
+import path from "node:path";
+import { Command } from "commander";
+import { compile } from "@flam/compiler";
+
+const program = new Command();
+
+program.name("flam").description("Compile and output your .flam files").version("0.0.1");
+program
+	.command("compile")
+	.description("Run the compiler against your entry file")
+	.argument("<file>", "The entry file to compile")
+	.option("-o, --output <file>", "Path to which the compiled output will be written")
+	.option("-i, --input <file>", "Path to the input file in JSON format")
+	.action((entryPath: string, { input, output }) => {
+		let ctx = {};
+
+		if (input) {
+			const absInput = path.resolve(process.cwd(), input);
+			console.log(absInput);
+			ctx = JSON.parse(readFileSync(absInput).toString());
+		}
+
+		const absPath = path.resolve(process.cwd(), entryPath);
+		const compiled = compile(absPath, ctx);
+
+		if (output) {
+			const absOutput = path.resolve(process.cwd(), output);
+			writeFileSync(absOutput, compiled);
+		} else {
+			console.log(compiled);
+		}
+	});
+
+program.parse();
--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -1,10 +1,8 @@
-import { readFileSync, writeFileSync } from "node:fs";
+import { readFileSync } from "node:fs";
 import { Compiler } from "./compiler";
 import { Scope } from "./scope";
 import { Parser } from "./parser";
 import { Tokenizer } from "./tokenizer";
-import path from "node:path";
-import { Command } from "commander";
 
 export function compile(absPath: string, ctx: Record<string, unknown>): string {
 	const file = readFileSync(absPath).toString();
@@ -15,34 +13,3 @@ export function compile(absPath: string, ctx: Record<string, unknown>): string {
 
 	return compiler.compile();
 }
-
-const program = new Command();
-
-program.name("flam").description("Compile and output your .flam files").version("0.0.1");
-program
-	.command("compile")
-	.description("Run the compiler against your entry file")
-	.argument("<file>", "The entry file to compile")
-	.option("-o, --output <file>", "Path to which the compiled output will be written")
-	.option("-i, --input <file>", "Path to the input file in JSON format")
-	.action((entryPath: string, { input, output }) => {
-		let ctx = {};
-
-		if (input) {
-			const absInput = path.resolve(process.cwd(), input);
-			console.log(absInput);
-			ctx = JSON.parse(readFileSync(absInput).toString());
-		}
-
-		const absPath = path.resolve(process.cwd(), entryPath);
-		const compiled = compile(absPath, ctx);
-
-		if (output) {
-			const absOutput = path.resolve(process.cwd(), output);
-			writeFileSync(absOutput, compiled);
-		} else {
-			console.log(compiled);
-		}
-	});
-
-program.parse();
